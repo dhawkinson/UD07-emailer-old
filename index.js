8512a642a0ac62cf5b90c7539e990d54b7688d2f
Doug Hawkinson <doug5solas@gmail.com>
2018-12-11 10:53:28 -0800

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app     = express();              //  NOTE: to self this is an instance of an express application - named 'app'
 
 app.get('/', (req, res) => {
-    res.send( { hi: 'there' } );
+    res.send( { hi: 'World' } );
 })
 
 const PORT = process.env.PORT || 5000;
