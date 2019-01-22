0e05109db21a9e44854c3a0c9669009ce1e9f464
Doug Hawkinson <doug5solas@gmail.com>
2019-01-22 10:49:20 -0800

    Tweak app/index.js for missing quote mark

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if ( process.env.NODE_ENV === 'production' ) {
     //  Express will serve up the index.html file if it does recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build, 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
